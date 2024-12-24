f0a6f111c2ec807608fcd445c30a7d63c23c84ed
groteherman <groteherman@gmail.com>
2024-12-24 16:28:39 +0100

    rotary encoder ipv potmeter

--- NanoGateDelay/lib/NanoGateDelay/NanoGateDelay.h
+++ NanoGateDelay/lib/NanoGateDelay/NanoGateDelay.h
@@ -4,6 +4,7 @@
 #define LED_PIN 13
 #define GATEIN_PIN 3
 #define GATEOUT_PIN 4
+#define SWITCH_PIN 5
 #define MAXARRAY 1024
 
 uint16_t getByteIndex(uint16_t);
--- NanoGateDelay/src/main.cpp
+++ NanoGateDelay/src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
+#include <RotaryEncoder.h>
 #include "NanoGateDelay.h"
+
+#define PIN_IN1 A2
+#define PIN_IN2 A3
+#define LEDSTRIP1_PIN 9
 
 uint8_t delayArr[MAXARRAY] = {};
 uint16_t maxDelay = MAXARRAY * 8;
 uint16_t actualDelay = maxDelay;
 volatile uint16_t bitIndex;
+bool justPressed;
+
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
 
 ISR(TIMER1_COMPA_vect)
 {
@@ -26,28 +34,49 @@ void setup() {
 
   pinMode(GATEIN_PIN, INPUT);
   pinMode(GATEOUT_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(LEDSTRIP1_PIN, OUTPUT);
 
   bitIndex = 0;
   // set up Timer 1
   TCCR1A = 0;          // normal operation
   //125 µS, giving a frequency of 1/0.000125 = 8 KHz
   TCCR1B = bit(WGM12) | bit(CS10);   // CTC, no pre-scaling
-  OCR1A =  15984;       // compare A register value (1000 * clock speed / 1024)   15984 = 1kHz
+  OCR1A = 15984;       // compare A register value (1000 * clock speed / 1024)   15984 = 1kHz
   TIMSK1 = bit (OCIE1A);             // interrupt on Compare A Match
+
+  justPressed = false;
 }
 
 void loop() {
-  unsigned long sum = 0;
+  static int pos = 0;
+  encoder.tick();
 
-  for (byte i=0; i<16; i++){
-    sum += analogRead(A0);
+  bool isPressed = !digitalRead(SWITCH_PIN);
+  if (isPressed){
+    if(!justPressed){
+      digitalWrite (LEDSTRIP1_PIN, HIGH);
+      justPressed = true;
+      if (actualDelay > 4){
+        actualDelay = actualDelay >> 2;
+      } else {
+        actualDelay = maxDelay;
+      }
+    }
+  } else {
+    justPressed = false;
   }
 
-  uint16_t newDelay = sum>>1;
-  if (abs(newDelay - actualDelay) > 8){
-    actualDelay = newDelay;
+  int newPos = encoder.getPosition();
+  if (pos != newPos) {
+    Serial.print("pos:");
+    Serial.print(newPos);
+    Serial.print(" dir:");
+    Serial.println((int)(encoder.getDirection()));
+    pos = newPos;
+    OCR1A = 15984 + 50 * pos;
   }
-
   Serial.println(actualDelay);
+  digitalWrite (LEDSTRIP1_PIN, LOW);
 }
