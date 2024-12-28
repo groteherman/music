cb6b0ae2819e748d4e2b6ea806e27e96e80677dd
groteherman <groteherman@gmail.com>
2024-12-28 21:47:09 +0100

    better tests

--- NanoGateDelay/test/test_native/test.cpp
+++ NanoGateDelay/test/test_native/test.cpp
@@ -23,21 +23,37 @@ void test_bitIndex(){
     TEST_ASSERT_EQUAL_INT16(getBitIndex(8), 0); 
 }
 
-void test_getAndSetBit(){
+void test_getAndSetBit_init(){
     uint8_t delayArr[2];
-    delayArr[0] = 0;
-    delayArr[1] = 0;
     uint16_t maxDelay = 2 * 8;
     
     for (int16_t i = 0; i < maxDelay; i++){
-        bool sut = getAndSetBit(i, i & 1, delayArr);
+        bool sut = getAndSetBit(i, 1, delayArr);
         TEST_ASSERT_FALSE(sut);
     }
 
     for (int16_t i = 0; i < maxDelay; i++){
-        bool sut = getAndSetBit(i, i & 1, delayArr);
-        TEST_ASSERT(sut == (i & 1));
+        bool sut = getAndSetBit(i, 1, delayArr);
+        TEST_ASSERT_TRUE(sut);
     }
+}
+
+void test_getAndSetBit_value(){
+    uint8_t delayArr[2];
+    uint16_t maxDelay = 2 * 8;
+   
+    delayArr[0] = 0b11110000;
+    delayArr[1] = 0b10101010;
+
+    bool val = 0;
+    
+    for (int16_t i = 0; i < maxDelay; i++){
+        val = getAndSetBit(i, val, delayArr);
+    }
+
+    TEST_ASSERT(val = 1);
+    TEST_ASSERT(delayArr[0] = 0b11100000);
+    TEST_ASSERT(delayArr[1] = 0b01010101);
 }
 
 int main( int argc, char **argv) {
@@ -45,7 +61,8 @@ int main( int argc, char **argv) {
 
     RUN_TEST(test_byteIndex);
     RUN_TEST(test_bitIndex);
-    RUN_TEST(test_getAndSetBit);
+    RUN_TEST(test_getAndSetBit_init);
+    RUN_TEST(test_getAndSetBit_value);
 
     UNITY_END();
 }
